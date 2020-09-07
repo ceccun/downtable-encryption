10b04e8c23f74fba94c57be89c7499a85d2564f9
stel-la <59512425+stel-la@users.noreply.github.com>
2020-09-07 22:23:08 +0100

    Update downtable.js

--- downtable.js
+++ downtable.js
@@ -3,7 +3,7 @@
 function encrypt(data, key){
     var sourcemap = "abcdefghijklmnopqrstuvwxyz ABCEFGHIJKLMNOPQRSTUVWXYZ`¬!£$%^&*()_+1234567890-=[]{};'#:@~,./<>?\|"
 var table = sourcemap.split("");
-var datad = data;
+var datad = btoa(data);
 var ackey = key;
 var keyusage = "";
 var mappedkey = [];
@@ -50,7 +50,7 @@ var datamapped = datad.split(":");
 var output = "";
 datamapped.splice(-1,1)
 datamapped.forEach(decoder);
-return output;
+return atob(output);
 
 function decoder(item, index){
     var indexintab = mappedkey.indexOf(item);
